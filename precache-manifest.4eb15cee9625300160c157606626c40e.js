a84ad8b7ce28d858bb107f61048c8c70bf14938c
achaq <abdelkarimachaq@gmail.com>
2020-08-05 21:03:13 +0200

    Updates

---
+++ precache-manifest.4eb15cee9625300160c157606626c40e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1b325a6de14b07d2e35b5c7344607d",
+    "url": "/courses/index.html"
+  },
+  {
+    "revision": "40d44c8142f985e412c9",
+    "url": "/courses/static/css/main.964e1dcf.chunk.css"
+  },
+  {
+    "revision": "b7922c2f8dc00dad9e57",
+    "url": "/courses/static/js/2.94cca514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/courses/static/js/2.94cca514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d44c8142f985e412c9",
+    "url": "/courses/static/js/main.048894ea.chunk.js"
+  },
+  {
+    "revision": "d4d26fb434ba54fa9270",
+    "url": "/courses/static/js/runtime-main.8d00cf69.js"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/courses/static/media/Pacifico-Regular.9b94499c.ttf"
+  }
+]);
